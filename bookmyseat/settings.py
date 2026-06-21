ec4663ef888f7b45116e611b0d08fac78a77a7de
code-ankisha <ankishamewal@gmail.com>
2026-06-22 00:27:04 +0530

    changes in env file

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -144,3 +144,11 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
